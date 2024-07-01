7a36c4bf78cb8d799b4a3f76025c02631df3ab91
Poxie <poxen12@hotmail.com>
2024-07-01 19:09:48 +0200

    Responsive navbar

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,4 +1,6 @@
 "use client";
+import ArrowIcon from "@/icons/ArrowIcon";
+import HamIcon from "@/icons/HamIcon";
 import HeartIcon from "@/icons/HeartIcon"
 import Image from "next/image"
 import Link from "next/link"
@@ -15,6 +17,7 @@ const NAVBAR_TABS = [
 const SCROLL_THRESHOLD = 150;
 export default function Navbar() {
     const [hasScrolled, setHasScrolled] = useState(false);
+    const [menuOpen, setMenuOpen] = useState(false);
 
     useEffect(() => {
         const onScroll = () => {
@@ -30,15 +33,24 @@ export default function Navbar() {
 
         window.addEventListener('scroll', onScroll);
         return () => window.removeEventListener('scroll', onScroll);
-    })
+    }, []);
+
+    const toggleMenu = () => setMenuOpen(prev => !prev);
 
     return(
         <header className={twMerge(
             "z-50 left-0 -top-[100px] w-full bg-primary transition-[top] duration-500",
             hasScrolled && "shadow-md sticky top-0",
         )}>
-            <div className="main-width py-5 grid items-center grid-cols-8">
-                <div className="flex col-span-2">
+            <div className="main-width py-5 grid items-center gap-10 grid-cols-3 lg:grid-cols-8">
+                <div className="flex items-center gap-3 col-span-2">
+                    <button
+                        onClick={toggleMenu}
+                        className="block lg:hidden"
+                        aria-label="Öppna meny"
+                    >
+                        <HamIcon size={24} />
+                    </button>
                     <Link 
                         href="/"
                         aria-label="Hem"
@@ -51,18 +63,43 @@ export default function Navbar() {
                         />
                     </Link>
                 </div>
-                <nav className="col-span-4">
-                    <ul className="flex justify-center gap-4">
+                <nav className={twMerge(
+                    "col-span-4 z-10 fixed top-0 right-full h-full w-full bg-primary transition-[right] duration-500",
+                    "lg:relative lg:w-[unset] lg:h-[unset] lg:right-[unset]",
+                    menuOpen && "right-0",
+                )}>
+                    <ul className={twMerge(
+                        "flex flex-col justify-center divide-y-[1px] divide-tertiary",
+                        "lg:flex-row lg:gap-4 lg:divide-y-0",
+                    )}>
+                        <li className="block lg:hidden">
+                            <button 
+                                onClick={toggleMenu}
+                                className="p-4 flex items-center gap-2"
+                                aria-label={'Stäng meny'}
+                            >
+                                <HamIcon className="mb-0.5" size={24} />
+                                Stäng
+                            </button>
+                        </li>
                         {NAVBAR_TABS.map(tab => (
                             <li>
-                                <Link href={tab.path}>
+                                <Link 
+                                    className={twMerge(
+                                        "p-4 flex items-center justify-between text-xl",
+                                        "lg:p-0 lg:text-base",
+                                    )}
+                                    href={tab.path}
+                                >
                                     {tab.text}
+
+                                    <ArrowIcon className="-rotate-90 block lg:hidden" size={20} />
                                 </Link>
                             </li>
                         ))}
                     </ul>
                 </nav>
-                <div className="flex justify-end col-span-2">
+                <div className="flex justify-end lg:col-span-2">
                     <button 
                         className="flex items-center gap-2"
                         aria-label="Favoriter"
