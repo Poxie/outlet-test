1110c746f962bcf57bab7d804088ab082bd0a467
Poxie <poxen12@hotmail.com>
2024-07-01 05:17:57 +0200

    Responsive hero text

--- components/home/hero/index.tsx
+++ components/home/hero/index.tsx
@@ -7,10 +7,10 @@ export default function HomeHero() {
                 className="relative after:absolute after:top-0 after:left-0 after:w-full after:h-full after:bg-black/70"
             />
             <div className="w-[550px] max-w-main absolute left-2/4 top-2/4 -translate-x-2/4 -translate-y-2/4">
-                <h1 className="text-6xl font-semibold text-center text-light">
+                <h1 className="text-5xl md:text-6xl font-semibold text-center text-light">
                     30 - 70% rabatt
                 </h1>
-                <p className="mt-6 text-3xl text-center text-light">
+                <p className="mt-4 sm:mt-6 text-xl sm:text-2xl md:text-3xl text-center text-light">
                     Fynda varor med kända varumärken med upp till 70% rabatt.
                 </p>
             </div>
