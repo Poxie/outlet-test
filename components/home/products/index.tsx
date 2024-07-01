dd3fecfb9f1c050f2281d0074a0801e2b11d24bb
Poxie <poxen12@hotmail.com>
2024-07-02 00:21:17 +0200

    Add more summer products

--- components/home/products/index.tsx
+++ components/home/products/index.tsx
@@ -1,8 +1,7 @@
+// This would be replaced with server side rendering & fetching data from a database
+import categories from '@/assets/json/categories.json';
 import Link from "next/link";
 import HomeProductRow from "./HomeProductRow";
-
-// The products along with their images & category should be fetched from the API
-const PRODUCT_IDS = ['1','2','3','4','5','6','7'];
 
 export default function HomeProducts() {
     return(
@@ -10,18 +9,13 @@ export default function HomeProducts() {
             <h2 className="mb-10 text-3xl text-center">
                 Upptäck vårt sortiment
             </h2>
-            <HomeProductRow 
-                title="Sommar"
-                productIds={PRODUCT_IDS}
-            />
-            <HomeProductRow 
-                title="Sommarmode"
-                productIds={PRODUCT_IDS}
-            />
-            <HomeProductRow 
-                title="Uteplatsen"
-                productIds={PRODUCT_IDS}
-            />
+            {categories.map(category => (
+                <HomeProductRow 
+                    title={category.title}
+                    productIds={category.products}
+                    key={category.categoryId}
+                />
+            ))}
             <div className="mt-8 flex justify-center relative">
                 <Link
                     className="px-5 bg-primary after:z-[-1] after:absolute after:top-2/4 after:left-0 after:-translate-y-2/4 after:w-full after:h-[1px] after:bg-tertiary"
