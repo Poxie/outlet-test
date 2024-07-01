be9a01c12337b1b6ea8b2dcfb5b7c92d1520f6de
Poxie <poxen12@hotmail.com>
2024-07-01 16:18:56 +0200

    Add footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body className={montserrat.className}>
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
