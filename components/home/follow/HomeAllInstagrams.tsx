603172d184a2ac9a2383c97fdbdf1701ef5e18e6
Poxie <poxen12@hotmail.com>
2024-07-01 18:05:20 +0200

    Change instagram cards container column count on smaller devices

--- components/home/follow/HomeAllInstagrams.tsx
+++ components/home/follow/HomeAllInstagrams.tsx
@@ -8,7 +8,7 @@ export default function HomeAllInstagrams() {
             <h2 className="mt-8 mb-4 text-xl">
                 Följ din lokala Outlet
             </h2>
-            <ul className="grid grid-cols-5 gap-2">
+            <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-2">
                 {ACCOUNTS.map(account => {
                     const accountId = account
                         .toLowerCase()
