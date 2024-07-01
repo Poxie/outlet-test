2aa4c52684160851415d72dd214161cf01c610e7
Poxie <poxen12@hotmail.com>
2024-07-02 00:30:02 +0200

    Make sickla notice responsive

--- components/sickla-notice/index.tsx
+++ components/sickla-notice/index.tsx
@@ -4,23 +4,23 @@ export default function SicklaNotice() {
     const sicklaPhoneNumber = "073-052 93 74";
     return(
         <div className="py-6 border-y-[1px] border-y-tertiary">
-            <div className="main-width flex items-center justify-between">
-                <div className="flex items-center gap-2">
-                    <WarningIcon size={24} />
+            <div className="main-width flex gap-4 sm:gap-2">
+                <WarningIcon className="mt-0.5 sm:mt-0 min-w-6" size={24} />
+                <div className="flex-1 flex flex-col sm:flex-row gap-2">
                     <span>
                         Notera att utbudet hos Sickla kan variera. Undrar du om Sickla har en produkt, ring först. 
                     </span>
+                    <span className="sm:ml-auto whitespace-nowrap">
+                        Tel:
+                        {' '}
+                        <a 
+                            href={`tel:${sicklaPhoneNumber}`}
+                            className="border-b-[1px] border-b-transparent hover:border-b-current transition-colors"
+                        >
+                            {sicklaPhoneNumber}
+                        </a>
+                    </span>
                 </div>
-                <span>
-                    Tel:
-                    {' '}
-                    <a 
-                        href={`tel:${sicklaPhoneNumber}`}
-                        className="border-b-[1px] border-b-transparent hover:border-b-current transition-colors"
-                    >
-                        {sicklaPhoneNumber}
-                    </a>
-                </span>
             </div>
         </div>
     )
