4e48c72e8c07983f05dd73daf226545ea08edaf4
Poxie <poxen12@hotmail.com>
2024-07-06 20:42:18 +0200

    Add active state to sidebar item

--- admin/components/sidebar/SidebarGroupItem.tsx
+++ admin/components/sidebar/SidebarGroupItem.tsx
@@ -1,13 +1,21 @@
 import Link from "next/link";
 import { SidebarItem } from "./SidebarGroups"
+import { usePathname } from "next/navigation";
+import { twMerge } from "tailwind-merge";
 
 export default function SidebarGroupItem({ item: { text, path, icon } }: {
     item: SidebarItem;
 }) {
+    const pathname = usePathname();
+    const isActive = pathname === path;
+
     return(
         <Link
             href={path}
-            className="p-2.5 -mx-2.5 flex items-center gap-3 text-muted hover:text-primary hover:bg-secondary active:bg-tertiary transition-colors rounded-md"
+            className={twMerge(
+                "p-2.5 -mx-2.5 flex items-center gap-3 text-muted hover:text-primary hover:bg-secondary active:bg-tertiary transition-colors rounded-md",
+                isActive && "text-primary bg-secondary font-medium",
+            )}
         >
             {icon}
             {text}
