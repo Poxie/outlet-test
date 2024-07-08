136f79e9b580afd9dc5de87fb5b14184e9acda28
Poxie <poxen12@hotmail.com>
2024-07-08 02:19:26 +0200

    Add different types of buttons

--- admin/components/button/index.tsx
+++ admin/components/button/index.tsx
@@ -1,21 +1,41 @@
+import Link from "next/link";
 import { twMerge } from "tailwind-merge";
 
-export default function Button({ children, className, disabled, onClick }: {
+export default function Button({ children, className, disabled, onClick, href, type='primary' }: {
     children: React.ReactNode;
     className?: string;
     onClick?: () => void;
     disabled?: boolean;
+    type?: 'primary' | 'secondary';
+    href?: string;
 }) {
+    className = twMerge(
+        "p-4 transition-colors rounded-md",
+        type === 'primary' && 'text-light bg-c-primary hover:bg-c-primary-accent',
+        type === 'secondary' && 'text-primary bg-secondary hover:bg-tertiary',
+        disabled && 'bg-c-primary-accent cursor-not-allowed',
+        className,
+    )
+
+    const props = {
+        className,
+        onClick,
+        disabled,
+    }
+
+    if(href) {
+        return(
+            <Link
+                href={href}
+                {...props}
+            >
+                {children}
+            </Link>
+        )
+    }
+
     return(
-        <button
-            className={twMerge(
-                "p-4 text-light bg-c-primary hover:bg-c-primary-accent transition-colors rounded-md",
-                disabled && 'bg-c-primary-accent cursor-not-allowed',
-                className,
-            )}
-            onClick={onClick}
-            disabled={disabled}
-        >
+        <button {...props}>
             {children}
         </button>
     )
