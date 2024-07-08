b23099dd2611d714595051cb6b27daeda7a4152d
Poxie <poxen12@hotmail.com>
2024-07-09 00:58:52 +0200

    Add label option to input component

--- admin/components/input/index.tsx
+++ admin/components/input/index.tsx
@@ -1,19 +1,31 @@
 import { HTMLInputTypeAttribute } from "react";
 import { twMerge } from "tailwind-merge";
 
-export default function Input({ onChange, className, placeholder, value, type='text' }: {
+export default function Input({ onChange, className, placeholder, value, label, type='text' }: {
     className?: string;
     placeholder?: string;
     onChange: (text: string) => void;
     type?: HTMLInputTypeAttribute;
+    label?: string
     value?: string;
 }) {
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         onChange(e.target.value);
     }
 
+    const inputId = label?.toLowerCase().replaceAll(' ', '-');
     return(
+        <>
+        {label && (
+            <label 
+                htmlFor={inputId}
+                className="mb-1 block font-medium"
+            >
+                {label}
+            </label>
+        )}
         <input 
+            id={inputId}
             type={type}
             value={value}
             onChange={handleChange}
@@ -23,5 +35,6 @@ export default function Input({ onChange, className, placeholder, value, type='t
             )}
             placeholder={placeholder}
         />
+        </>
     )
 }
