968657b58efac80bfd780c13faf68e3d8ca5a647
Poxie <poxen12@hotmail.com>
2024-07-09 19:10:44 +0200

    Add missing keys

--- admin/components/menu/MenuGroups.tsx
+++ admin/components/menu/MenuGroups.tsx
@@ -7,8 +7,11 @@ export default function MenuGroups({ groups, setOpen }: {
 }) {
     return(
         <ul className="min-w-menu p-1.5 absolute right-0 top-[calc(100%+.25rem)] divide-y-[1px] divide-tertiary border-[1px] border-tertiary bg-primary shadow rounded-md text-nowrap">
-            {groups.map(items => (
-                <li className="py-1.5 first:pt-0 last:pb-0">
+            {groups.map((items, key) => (
+                <li 
+                    className="py-1.5 first:pt-0 last:pb-0"
+                    key={key}
+                >
                     {items.map(item => (
                         <MenuItem 
                             item={item}
