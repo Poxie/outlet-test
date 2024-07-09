617f40823a434e8f9896dc663adb98d766a20abe
Poxie <poxen12@hotmail.com>
2024-07-09 20:11:01 +0200

    Add disabled prop to dropdown component

--- admin/components/dropdown/index.tsx
+++ admin/components/dropdown/index.tsx
@@ -10,10 +10,14 @@ export type DropdownItem = {
     text: string;
     href?: string;
 }
-export default function Dropdown({ activeItemId, items, onChange }: {
+export default function Dropdown({ activeItemId, items, onChange, className, label, disabled, disabledIcon }: {
     activeItemId: string;
     items: DropdownItem[];
     onChange?: (id: string) => void;
+    className?: string;
+    label?: string;
+    disabled?: boolean;
+    disabledIcon?: React.ReactNode;
 }) {
     const [isOpen, setIsOpen] = useState(false);
 
@@ -38,21 +42,40 @@ export default function Dropdown({ activeItemId, items, onChange }: {
 
     const toggleOpen = () => setIsOpen(!isOpen);
 
+    const labelId = label?.toLowerCase().replaceAll(' ', '-');
     const activeItem = items.find(item => item.id === activeItemId) || items[0];
     return(
         <div className="relative" ref={ref}>
+            {label && (
+                <label 
+                    htmlFor={labelId}
+                    className="mb-1 block font-medium"
+                >
+                    {label}
+                </label>
+            )}
             <button 
-                className="flex items-center gap-2"
+                id={labelId}
+                className={twMerge(
+                    "flex items-center justify-between gap-2 rounded-md transition-colors",
+                    className,
+                )}
                 onClick={toggleOpen}
+                disabled={disabled}
             >
                 {activeItem.text}
-                <ArrowIcon 
-                    size={18}
-                    className={twMerge(
-                        "rotate-90 transition-transform duration-300",
-                        isOpen && '-rotate-90',
-                    )}
-                />
+                {(!disabled || !disabledIcon) && (
+                    <ArrowIcon 
+                        size={18}
+                        className={twMerge(
+                            "rotate-90 transition-transform duration-300",
+                            isOpen && '-rotate-90',
+                        )}
+                    />
+                )}
+                {disabled && disabledIcon && (
+                    disabledIcon
+                )}
             </button>
             <AnimatePresence>
                 {isOpen && (
@@ -60,7 +83,7 @@ export default function Dropdown({ activeItemId, items, onChange }: {
                         initial={{ opacity: 0, scale: .95 }}
                         animate={{ opacity: 1, scale: 1 }}
                         exit={{ opacity: 0, scale: .95 }}
-                        className="w-[200px] p-2 absolute right-0 top-[calc(100%+.5rem)] bg-primary rounded-md shadow-lg"
+                        className="w-dropdown p-2 absolute right-0 top-[calc(100%+.5rem)] bg-primary border-[1px] border-tertiary rounded-md shadow-lg"
                     >
                         {items.map(item => {
                             const props = {
