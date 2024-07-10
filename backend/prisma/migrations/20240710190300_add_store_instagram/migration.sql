e1639131d0c5427ee216da6e827f559421e4a069
Poxie <poxen12@hotmail.com>
2024-07-10 21:04:54 +0200

    Add instagram URL to store model

---
+++ backend/prisma/migrations/20240710190300_add_store_instagram/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - Added the required column `instagramURL` to the `Store` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE `store` ADD COLUMN `instagramURL` VARCHAR(191) NOT NULL;
