5637965104fc8770ca4e4f8d10a6d00605b14900
Poxie <poxen12@hotmail.com>
2024-07-10 16:18:23 +0200

    Lock store inputs for non admins

--- admin/components/stores/store/StoreContactInformation.tsx
+++ admin/components/stores/store/StoreContactInformation.tsx
@@ -1,10 +1,15 @@
 import Input from "@/components/input";
+import useCurrentUser from "@/hooks/useCurrentUser";
+import { ADMIN_ROLE } from "@/utils/constants";
 import { Store } from "@/utils/types";
 
 export default function StoreContactInformation({ store, updateProps }: {
     store: Store;
     updateProps: (changes: Partial<Store>) => void;
 }) {
+    const { data: currentUser } = useCurrentUser();
+
+    const isAdmin = currentUser?.role === ADMIN_ROLE;
     return(
         <div className="flex gap-3">
             <Input 
@@ -13,6 +18,7 @@ export default function StoreContactInformation({ store, updateProps }: {
                 value={store.email}
                 onChange={email => updateProps({ email })}
                 containerClassName="flex-1"
+                disabled={!isAdmin}
             />
             <Input 
                 label="Phone number"
@@ -20,6 +26,7 @@ export default function StoreContactInformation({ store, updateProps }: {
                 value={store.phoneNumber}
                 onChange={phoneNumber => updateProps({ phoneNumber })}
                 containerClassName="flex-1"
+                disabled={!isAdmin}
             />
         </div>
     )
--- admin/components/stores/store/StoreDetails.tsx
+++ admin/components/stores/store/StoreDetails.tsx
@@ -1,16 +1,22 @@
 import Input from "@/components/input";
+import useCurrentUser from "@/hooks/useCurrentUser";
+import { ADMIN_ROLE } from "@/utils/constants";
 import { Store } from "@/utils/types"
 
 export default function StoreDetails({ store, updateProps }: {
     store: Store;
     updateProps: (changes: Partial<Store>) => void;
 }) {
+    const { data: currentUser } = useCurrentUser();
+
+    const isAdmin = currentUser?.role === ADMIN_ROLE;
     return(
         <>
         <Input 
             label="Store name"
             value={store.name}
             onChange={name => updateProps({ name })}
+            disabled={!isAdmin}
         />
         <Input 
             containerClassName="mt-3"
@@ -18,6 +24,7 @@ export default function StoreDetails({ store, updateProps }: {
             value={store.address}
             onChange={address => updateProps({ address })}
             textArea
+            disabled={!isAdmin}
         />
         </>
     )
