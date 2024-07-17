7eb244b3bbcd3e84b97918e2e6ab65df9f6f688c
Poxie <poxen12@hotmail.com>
2024-07-17 02:15:54 +0200

    Allow parentId to be null when creating product groups

--- backend/src/validation/productGroupSchemas.ts
+++ backend/src/validation/productGroupSchemas.ts
@@ -4,5 +4,5 @@ export const createProductGroupSchema = z.object({
     name: z.string().min(1).max(255),
     description: z.string().min(1).max(255),
     banner: z.string(),
-    parentId: z.string().optional(),
+    parentId: z.string().optional().nullable(),
 })
