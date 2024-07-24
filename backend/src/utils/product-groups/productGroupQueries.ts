d1eaf856beef9d5b083cab47de6aee0d315755fd
Poxie <poxen12@hotmail.com>
2024-07-24 15:44:33 +0200

    Temporary removing product group caching, first determine what data is frequently accessed by users

--- backend/src/utils/product-groups/productGroupQueries.ts
+++ backend/src/utils/product-groups/productGroupQueries.ts
@@ -8,27 +8,15 @@ import { ProductGroup } from "@prisma/client";
 
 export default class ProductGroupQueries {
     static async getProductGroups(withProducts = false) {
-        const cacheKey = REDIS_KEYS.productGroups(withProducts);
-
-        const redisGroups = await RedisHandler.get<ProductGroup[]>(cacheKey);
-        if(redisGroups) return redisGroups;
-
         const groups = await client.productGroup.findMany(
             IncludeGroupProps({ products: withProducts })
         );
         
         const groupsWithCounts = groups.map(ProductGroupUtils.transformGroup);
-
-        await RedisHandler.set(cacheKey, groupsWithCounts, REDIS_TAGS.productGroups);
-
+        
         return groupsWithCounts;
     }
     static async getProductGroupById(id: string, withProducts = false) {
-        const cacheKey = REDIS_KEYS.productGroup(id, withProducts);
-
-        const redisGroup = await RedisHandler.get<ProductGroup>(cacheKey);
-        if(redisGroup) return redisGroup;
-
         const group = await client.productGroup.findUnique({
             where: {
                 id,
@@ -39,16 +27,9 @@ export default class ProductGroupQueries {
 
         const groupWithCount = ProductGroupUtils.transformGroup(group);
 
-        await RedisHandler.set(cacheKey, groupWithCount, REDIS_TAGS.productGroups);
-
         return groupWithCount;
     }
     static async getProductGroupsByParentId(parentId: string) {
-        const cacheKey = REDIS_KEYS.productGroupsByParentId(parentId);
-
-        const redisGroups = await RedisHandler.get<ProductGroup[]>(cacheKey);
-        if(redisGroups) return redisGroups;
-
         const groups = await client.productGroup.findMany({
             where: {
                 parentId,
@@ -56,26 +37,6 @@ export default class ProductGroupQueries {
             ...IncludeGroupProps(),
         });
         const groupsWithCounts = groups.map(ProductGroupUtils.transformGroup);
-
-        await RedisHandler.set(cacheKey, groupsWithCounts, REDIS_TAGS.productGroups);
-
-        return groupsWithCounts;
-    }
-    static async getUnassignedProductGroups() {
-        const cacheKey = REDIS_KEYS.productGroupsByParentId(null);
-
-        const redisGroups = await RedisHandler.get(cacheKey);
-        if(redisGroups) return redisGroups;
-
-        const groups = await client.productGroup.findMany({
-            where: {
-                parentId: null,
-            },
-            ...IncludeGroupProps(),
-        });
-        const groupsWithCounts = groups.map(ProductGroupUtils.transformGroup);
-
-        await RedisHandler.set(cacheKey, groupsWithCounts, REDIS_TAGS.productGroups);
 
         return groupsWithCounts;
     }
