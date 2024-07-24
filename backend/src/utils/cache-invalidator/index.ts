d4d37b5f6cbd2b64bad8aecb7dd50fffe0dadbf8
Poxie <poxen12@hotmail.com>
2024-07-24 19:35:30 +0200

    Debouncing multiple product page invalidations

--- backend/src/utils/cache-invalidator/index.ts
+++ backend/src/utils/cache-invalidator/index.ts
@@ -2,12 +2,27 @@ import RedisHandler from "../redis/redisHandler";
 import REDIS_KEYS from "../redis/redisKeys";
 
 export default class CacheInvalidator {
+    private static debounceDelay = 1000;
+    private static debounceMap: Map<string, NodeJS.Timeout> = new Map();
+
+    private static debounceInvalidation(redisKeys: string[], key: string) {
+        if(this.debounceMap.has(key)) {
+            clearTimeout(this.debounceMap.get(key));
+        }
+
+        this.debounceMap.set(key, setTimeout(() => {
+            Promise.all(redisKeys.map(async redisKey => await RedisHandler.del(redisKey)));
+            this.debounceMap.delete(key);
+        }, this.debounceDelay));
+    }
+
     static async invalidateStores() {
         await RedisHandler.del(REDIS_KEYS.stores);
     }
     static async invalidateProductPage(id: string) {
-        const invalidatePage = RedisHandler.del(REDIS_KEYS.productPage(id));
-        const invalidateProductList = RedisHandler.del(REDIS_KEYS.productList);
-        await Promise.all([invalidatePage, invalidateProductList]);
+        this.debounceInvalidation(
+            [REDIS_KEYS.productList, REDIS_KEYS.productPage(id)], 
+            `productPage:${id}`,
+        );
     }
 }
