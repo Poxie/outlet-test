483f8e8b431c94a3d5a707daab4696992d5c3d79
Poxie <poxen12@hotmail.com>
2024-07-28 22:16:44 +0200

    Span main page visit card skeleton

--- admin/components/skeletons/PageVisitsSkeleton.tsx
+++ admin/components/skeletons/PageVisitsSkeleton.tsx
@@ -12,7 +12,7 @@ export default function PageVisitsSkeleton() {
         <Section className="p-0 grid md:grid-cols-2 xl:grid-cols-3 divide-y-[1px] divide-x-[1px] divide-tertiary">
             {PAGE_VISIT_CARD_TITLES.map((title, index) => (
                 <div
-                    className="p-5 flex justify-between items-center"
+                    className="p-5 flex justify-between items-center md:first:col-span-2 xl:first:col-span-1"
                     key={index}
                 >
                     <AnalyticsCardTitleSkeleton 
