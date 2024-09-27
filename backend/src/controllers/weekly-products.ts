464e2fec56b742c06f53e05fdf78c1a63ce95eed
Albin Kärvling <70824199+Poxie@users.noreply.github.com>
2024-09-27 16:15:02 +0200

    Emergency quick fix for weekly product query

--- backend/src/controllers/weekly-products.ts
+++ backend/src/controllers/weekly-products.ts
@@ -27,6 +27,18 @@ router.get('/:date', asyncHandler(async (req, res, next) => {
         throw new InvalidDealDateError();
     }
 
+    return res.json({ date: "2024-09-23", week: "54", group: {
+        id: "2024-09-23",
+        products: [
+            { id: "1", parentId: "2024-09-23", position: 0, imageURL: "https://outlet.poxen.dev/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdhmz2d926%2Fimage%2Fupload%2Fv1722461859%2Fgroups%2Ftill-koket%2Fproducts%2F29650390.png&w=750&q=75" },
+            { id: "2", parentId: "2024-09-23", position: 1, imageURL: "https://outlet.poxen.dev/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdhmz2d926%2Fimage%2Fupload%2Fv1722461856%2Fgroups%2Ftill-koket%2Fproducts%2F30872602.png&w=750&q=75" },
+            { id: "3", parentId: "2024-09-23", position: 2, imageURL: "https://outlet.poxen.dev/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdhmz2d926%2Fimage%2Fupload%2Fv1722461856%2Fgroups%2Ftill-koket%2Fproducts%2F33563388.png&w=750&q=75" },
+            { id: "4", parentId: "2024-09-23", position: 3, imageURL: "https://outlet.poxen.dev/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdhmz2d926%2Fimage%2Fupload%2Fv1722461856%2Fgroups%2Ftill-koket%2Fproducts%2F39704984.png&w=750&q=75" },
+            { id: "5", parentId: "2024-09-23", position: 4, imageURL: "https://outlet.poxen.dev/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdhmz2d926%2Fimage%2Fupload%2Fv1722461856%2Fgroups%2Ftill-koket%2Fproducts%2F48875035.png&w=750&q=75" },
+            { id: "6", parentId: "2024-09-23", position: 5, imageURL: "https://outlet.poxen.dev/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdhmz2d926%2Fimage%2Fupload%2Fv1722461856%2Fgroups%2Ftill-koket%2Fproducts%2F55839229.png&w=750&q=75" },
+        ]
+    } })
+    
     const group = await ProductGroupQueries.getProductGroupById(date, true, 'WEEKLY_PRODUCT');
     if(!group) {
         throw new ProductGroupNotFoundError();
